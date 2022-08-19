b77f162a8fe6f7fe003bd1b80ec3ad84146aa288
mariselvan21 <mari20041999@gmail.com>
2022-08-19 09:41:49 +0530

    add files

---
+++ main.js
@@ -0,0 +1,31 @@
+export var myNum=(value1,value2)=>{
+    var result=value1+value2;
+    return result;
+}
+var answer=myNum(5,10);
+// console.log(answer);
+
+var vechicle=(model,seat)=> model+" is "+seat+" seat vechicle";
+// console.log(vechicle("tata",4));
+
+var phone= model=>"My phone model is " + model;
+//console.log(phone("note11"));
+
+var calculation=value=>{
+    var answer=value+value;
+    return answer;
+}
+console.log(calculation(10));
+
+
+export var a=10;
+
+var b=10;
+export default a;
+
+
+
+
+
+
+
---
+++ main2.js
@@ -0,0 +1,11 @@
+import {a} from "./main.js";
+// console.log(a);
+
+import { myNum } from "./main.js";
+console.log(myNum);
+
+import c from "./main.js"
+console.log(c);
+
+import * as info from "./main.js"
+console.log(info.a);
